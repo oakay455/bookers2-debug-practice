7441c68c2d7a7ecc1247a67528f325116347bed4
oakay455 <ayako110.89@gmail.com>
2022-05-07 08:07:29 +0000

    [Add]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,11 +21,8 @@ class User < ApplicationRecord
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
 
-
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
-
-
 
 
   def get_profile_image
@@ -43,5 +40,17 @@ class User < ApplicationRecord
   # フォローしているか判定
   def following?(user)
     followings.include?(user)
+  end
+
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE?', content+'%')
+    elsif method == 'backward'
+      User.where('name LIKE?', '%'+content)
+    else
+      User.where('name LIKE?', '%'+content+'%')
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: "homes#top"
@@ -15,5 +13,8 @@ Rails.application.routes.draw do
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
   end
+
+  get "/search" => "searches#search"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
